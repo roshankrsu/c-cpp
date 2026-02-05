98983af47763ace87a75a643f1c19baa282a0f5a
Roshan <roshankrsu@gmail.com>
2026-02-05 18:02:39 +0530

    string

---
+++ string.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    string s = "roshan";
+    int len = s.size();
+    // s[len-1] = 't';
+    cout << s[len - 1];// prints last string
+}
