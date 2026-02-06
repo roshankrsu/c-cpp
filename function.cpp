492e070ce2256144f34d1f516ee5997d38fbfdbf
Roshan <roshankrsu@gmail.com>
2026-02-06 11:20:48 +0530

    functions

---
+++ function.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// void function -> which does not returns anything
+void printName(string name){
+    cout<< "hola "<< name << endl;
+}
+
+int main(){
+    string name; 
+    cin >> name;
+    printName(name);
+
+    string name2;
+    cin >> name2;
+    printName(name2);
+
+    return 0;
+}
